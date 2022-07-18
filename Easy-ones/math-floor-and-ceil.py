d8d333df4fd5f8ffd4e39da5a8b6e09d1c61a56e
si-shakil <sishakil850@gmail.com>
2022-07-18 23:13:50 +0600

    pb-3,4 added

---
+++ Easy-ones/math-floor-and-ceil.py
@@ -0,0 +1,6 @@
+import math
+
+float_num = float(input("Enter a float number: "))
+floor_of_float_num = math.floor(float_num)
+ceil_of_float_num = math.ceil(float_num)
+print("The floor num is ", floor_of_float_num, "& the ceil number is ", ceil_of_float_num)
---
+++ Easy-ones/random-number.py
@@ -0,0 +1,4 @@
+import random
+
+random_num = random.randint(0,10)
+print(random_num)
