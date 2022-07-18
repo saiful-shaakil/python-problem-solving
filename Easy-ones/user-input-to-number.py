ed550e866067e85e410aaf99edf50a3a2d616563
si-shakil <sishakil850@gmail.com>
2022-07-18 22:34:55 +0600

    ps 1  solved

---
+++ Easy-ones/user-input-to-number.py
@@ -0,0 +1,6 @@
+int_text = input("Enter a integer number: ")
+float_text = input("Enter a float number: ")
+int_num = int(int_text)
+float_num = float(float_text)
+result = int_num * float_num
+print("Your result is", result)
